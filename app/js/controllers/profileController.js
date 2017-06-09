e679df58ecdd5b02205bbda8fd0fe3e06cd45fa8
Jehan-HENRY <jehan.henry@gmail.com>
2017-06-09 12:07:02 +0200

    profile ok for pres

--- app/js/controllers/profileController.js
+++ app/js/controllers/profileController.js
@@ -1,4 +1,17 @@
 angular.module('app')
-    .controller('ProfileController', function($scope, CurrentUser) {
-      $scope.user = CurrentUser.user();
-    });
+  .controller('ProfileController', function($scope, CurrentUser) {
+    $scope.user = CurrentUser.user();
+
+    $scope.originfos = false;
+    $scope.newinfos = false;
+
+    $scope.modifier = function () {
+      $scope.originfos = true;
+      $scope.newinfos = true;
+    };
+
+    $scope.pasmodifier = function () {
+      $scope.originfos = false;
+      $scope.newinfos = false;
+    };
+  });
